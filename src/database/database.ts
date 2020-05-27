2dad0ca69152e1a5e24cf87d7fcc167da7616e85
Gabiel Maya Sanchez <isc_gmayas@hotmail.com>
2020-05-27 12:33:59 -0500

    Commit 02

--- src/database/database.ts
+++ src/database/database.ts
@@ -6,8 +6,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const connectionString = `postgresql://${process.env.User}:${process.env.Password}@${process.env.Host}:${process.env.Port}/${process.env.Database}`
 
 const pool = new Pool({
-  connectionString: connectionString,
-  //connectionString: isProduction ? process.env.URI : connectionString,
+  connectionString: isProduction ? process.env.URI : connectionString,
   //connectionString: process.env.URI,
   ssl: true,
 })
